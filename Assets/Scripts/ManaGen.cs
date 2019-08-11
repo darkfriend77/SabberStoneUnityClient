cfb4b171e30f5335bb5dfe256eeab4902ea607be
darkfriend77 <darkfriend@swissonline.ch>
2019-08-11 19:50:03 +0200

    added the mana crystal again, they still share a mystery ...

--- Assets/Scripts/ManaGen.cs
+++ Assets/Scripts/ManaGen.cs
@@ -36,39 +36,6 @@ public class ManaGen : MonoBehaviour
 
     }
 
-    internal void UpdateGame(PowerPlayer powerPlayer)
-    {
-        int resources = powerPlayer.PowerEntity.Tags[GameTag.RESOURCES];
-        int resourcesTemp = powerPlayer.PowerEntity.Tags.ContainsKey(GameTag.TEMP_RESOURCES) ? powerPlayer.PowerEntity.Tags[GameTag.TEMP_RESOURCES] : 0;
-        int resourcesUsed = powerPlayer.PowerEntity.Tags.ContainsKey(GameTag.RESOURCES_USED) ? powerPlayer.PowerEntity.Tags[GameTag.RESOURCES_USED] : 0;
-
-        manaCurrent.text = (resources + resourcesTemp - resourcesUsed).ToString();
-        manaMax.text = (resources + resourcesTemp).ToString();
-
-        //while (manaCrystals.Count != resources + resourcesTemp)
-        //{
-        //    if (manaCrystals.Count < resources + resourcesTemp)
-        //    {
-        //        var crystal = Instantiate(ManaCrystalPrefab, manaBar);
-        //        manaCrystals.Add(crystal);
-        //        crystal.transform.GetComponent<ManaCrystalGen>().SetState(ManaCrystalState.EMPTY);
-        //    }
-        //    else if (manaCrystals.Count > resources + resourcesTemp)
-        //    {
-        //        var crystal = manaCrystals[manaCrystals.Count - 1];
-        //        manaCrystals.Remove(crystal);
-        //        Destroy(crystal);
-        //    }
-
-        //}
-
-        //for(int i = 0; i < manaCrystals.Count - 1 && i < resources + resourcesTemp - resourcesUsed; i++)
-        //{
-        //    manaCrystals[i].transform.GetComponent<ManaCrystalGen>().SetState(ManaCrystalState.FULL);
-        //}
-
-    }
-
     internal void Update(EntityExt entityExt)
     {
         int res = entityExt.Tags[GameTag.RESOURCES];
@@ -77,5 +44,29 @@ public class ManaGen : MonoBehaviour
 
         manaCurrent.text = (res + tmp - use).ToString();
         manaMax.text = (res + tmp).ToString();
+
+        while (manaCrystals.Count != res + tmp)
+        {
+            if (manaCrystals.Count < res + tmp)
+            {
+                var crystal = Instantiate(ManaCrystalPrefab, manaBar);
+                manaCrystals.Add(crystal);
+                crystal.transform.GetComponent<ManaCrystalGen>().SetState(ManaCrystalState.EMPTY);
+            }
+            else if (manaCrystals.Count > res + tmp)
+            {
+                var crystal = manaCrystals[manaCrystals.Count - 1];
+                manaCrystals.Remove(crystal);
+                Destroy(crystal);
+            }
+        }
+
+        for (int i = 0; i < manaCrystals.Count - 1; i++)
+        {
+            if (i + 1 <= res + tmp - use)
+            {
+                manaCrystals[i].transform.GetComponent<ManaCrystalGen>().SetState(ManaCrystalState.FULL);
+            }
+        }
     }
 }
