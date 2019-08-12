abdccf79ab8baf2534e4806fdfcc57f2804a9ffc
Pietro-coolshop <pietro@coolshop.it>
2019-08-12 23:13:23 +0200

    ultrabasic animation done

---
+++ Assets/AniamteInEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniamteInEditor : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        animator.Update(Time.deltaTime);
+    }
+
+}
---
+++ Assets/ParticleAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAttack : MonoBehaviour
+{
+
+    [SerializeField]
+    Animator controller;
+
+    [SerializeField]
+    GameObject missilePrefab;
+
+    GameObject missile;
+    Animator missileAnimator;
+
+    private float time;
+
+    public float attackInterval = 3f;
+    public float speed = 1f;
+
+    void Start() {
+        time = attackInterval;
+    }
+
+    void Update()
+    {
+        Spawn();
+        missileAnimator = missile.GetComponent<Animator>();
+        missileAnimator.speed = speed;
+    }
+
+    void Spawn()
+    {
+        float newTime = (time + Time.deltaTime * speed) % attackInterval;
+        if (newTime < time)
+        {
+            if(missile != null) Destroy(missile);
+            missile = Instantiate(missilePrefab);
+        }
+        time = newTime;
+    }
+
+    void OnDrawGizmos()
+    {
+    }
+}
